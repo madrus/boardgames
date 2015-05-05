298f1d364f01a625de11011857ca28c72b070c61
madrus <madrus@gmail.com>
2015-05-05 15:41:13 +0200

    Added static files and used render method

--- main/views.py
+++ main/views.py
@@ -2,4 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("Hello, World!")
+    #return HttpResponse("Hello, World!") # direct message string
+    #return render(request, "main/home.html") # render from template
+    return render(request, "main/home.html", {'message': 'Hi, here!'}) # render from template with dictionary
